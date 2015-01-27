685aaf4af2d729a34c3ba8f01132c27a53548fd8
mrmarktyy <mrmarktyy@gmail.com>
2015-01-27 16:39:29 +1100

    [FEATURE] Add build task for wechat

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,35 +1,55 @@
-module.exports = function (grunt) {
-    'use strict';
+module.exports = function(grunt) {
+  'use strict';
 
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
 
-        watch: {
-          sass: {
-            files: 'stylesheets/scss/**/*.scss',
-            tasks: ['sass:dist']
-          }
+    watch: {
+      sass: {
+        files: 'stylesheets/scss/**/*.scss',
+        tasks: ['sass:dist']
+      }
+    },
+
+    sass: {
+      dist: {
+        options: {
+          style: 'compact'
         },
+        files: [{
+          expand: true,
+          cwd: 'stylesheets/scss',
+          src: ['*.scss'],
+          dest: 'stylesheets/css',
+          ext: '.css'
+        }]
+      }
+    },
 
-        sass: {
-            dist: {
-                options: {
-                    style: 'compact'
-                },
-                files: [{
-                    expand: true,
-                    cwd: 'stylesheets/scss',
-                    src: ['*.scss'],
-                    dest: 'stylesheets/css',
-                    ext: '.css'
-                }]
-            }
+    uglify: {
+      dist: {
+        files: {
+          'dist/main.min.js': ['scripts/main.js']
         }
-    });
+      }
+    },
 
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-sass');
+    cssmin: {
+      dist: {
+        files: {
+          'dist/style.min.css': ['stylesheets/css/site.css', 'stylesheets/css/style.css']
+        }
+      }
+    }
 
-    grunt.registerTask('default', ['sass:dist', 'watch']);
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+  grunt.registerTask('default', ['sass:dist', 'watch']);
+  grunt.registerTask('build', ['sass:dist', 'cssmin:dist', 'uglify:dist']);
 
 };
