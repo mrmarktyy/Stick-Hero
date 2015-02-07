31c8a7d86140a8135114f035f551f4767fb02117
Mark Zhou <mrmarktyy@gmail.com>
2015-02-07 15:19:04 +1100

    [FEATURE] Dynamic share hero icon

--- scripts/m.js
+++ scripts/m.js
@@ -174,6 +174,7 @@ $(function() {
     this.switchHero = function (hero) {
       this.hero = parseInt(hero, 10) || this.hero;
       localStorage.setItem('hero', this.hero);
+      $('#wx_pic img').attr('src', 'images/hero' + this.hero + '.png');
 
       var HERO = HEROS[this.hero -1];
       HERO_WIDTH = Math.round(HERO[0] * WIDTH_RATIO);
