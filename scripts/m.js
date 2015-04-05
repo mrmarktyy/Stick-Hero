be84040111c49a4a8dbc25cd6af4c1b199aa66f0
Mark Zhou <mrmarktyy@gmail.com>
2015-04-05 10:41:27 +1000

    UPDATE

--- scripts/m.js
+++ scripts/m.js
@@ -30,63 +30,6 @@
   }
 }());
 
-var store = function store(key, value) {
-  'use strict';
-  var data;
-  var IS_ANDROID = navigator.userAgent.toLowerCase().indexOf('android') > -1;
-  var lsSupport = IS_ANDROID ? false : true;
-
-  if (typeof value !== 'undefined' && value !== null) {
-    if (typeof value === 'object' ) {
-      value = JSON.stringify(value);
-    }
-    if (lsSupport) {
-      localStorage.setItem(key, value);
-    } else {
-      createCookie(key, value, 100);
-    }
-  }
-
-  if (typeof value === 'undefined') {
-    if (lsSupport) {
-      data = localStorage.getItem(key);
-    } else {
-      var cookieData = readCookie(key);
-      data = cookieData !== null ? cookieData : localStorage.getItem(key);
-    }
-    try {
-     data = JSON.parse(data);
-    }
-    catch(e) {
-     data = data;
-    }
-    return data;
-  }
-  if (value === null) {
-    if (lsSupport) {
-      localStorage.removeItem(key);
-    } else {
-      createCookie(key, '', -1);
-    }
-  }
-  function createCookie(key, value, exp) {
-    var date = new Date();
-    date.setTime(date.getTime() + (exp * 24 * 60 * 60 * 1000));
-    var expires = '; expires=' + date.toGMTString();
-    document.cookie = key + '=' + value + expires + '; path=/';
-  }
-  function readCookie(key) {
-    var nameEQ = key + '=';
-    var ca = document.cookie.split(';');
-    for (var i = 0, max = ca.length; i < max; i++) {
-      var c = ca[i];
-      while (c.charAt(0) === ' ') c = c.substring(1, c.length);
-      if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
-    }
-    return null;
-  }
-};
-
 $(function() {
   'use strict';
 
